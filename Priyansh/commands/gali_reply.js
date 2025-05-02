af4076236db479bbea055faec5a9fe5754d39083
forhad1khanyt <mamunkhanyt963@gmail.com>
2025-05-02 17:36:22 +0600

    Update gali_reply.js

--- Priyansh/commands/gali_reply.js
+++ Priyansh/commands/gali_reply.js
@@ -3,9 +3,9 @@ module.exports.config = {
 	name: "gali",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
-	description: "no prefix",
-	commandCategory: "no prefix",
+	credits: "forhad khan", 
+	description: "bokachuda",
+	commandCategory: "tor bap bukachida",
 	usages: "abal",
     cooldowns: 5, 
 };
